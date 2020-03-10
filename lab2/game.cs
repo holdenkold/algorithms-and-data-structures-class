b3bda7dd1aa66fcb9de82074e7f6abb2d92a55ad
Palina Hrynko <01133991@pw.edu.pl>
2020-03-10 15:59:30 +0100

    Init commit

---
+++ lab2/game.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ASD
+{
+public class Game : MarshalByRefObject
+    {
+
+        /// <summary>
+        /// Zadanie na lab 2 z ASD2
+        /// </summary>
+        /// <param name="numbers">Zadany ciąg liczb</param>
+        /// <param name="moves">Przykładowa sekwencja ruchów</param>
+        /// <returns>Wynik gry dla gracza rozpoczynającego grę (suma zebranych liczb)</returns>
+        /// 
+
+
+    public int OptimalStrategy(int[] numbers, out int[] moves)
+        {
+            moves = null;
+            return 0;
+        }
+
+    }
+
+}
