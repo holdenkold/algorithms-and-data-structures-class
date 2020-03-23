a71f8420fbca69d3a1b6928644da6d1676d0afd8
Polina Grinko <grinkopolina@gmail.com>
2020-03-23 13:45:18 +0100

    Init lab2 commit

---
+++ lab3/Lab03.cs
@@ -0,0 +1,38 @@
+﻿
+namespace ASD
+{
+using ASD.Graphs;
+
+public class Lab03 : System.MarshalByRefObject
+    {
+
+    // czesc I
+    public Graph Square(Graph graph)
+        {
+        return null;
+        }
+
+    // czesc II
+    public Graph LineGraph(Graph graph, out (int x, int y)[] names)
+        {
+        names = null;
+        return null;
+        }
+
+    // czesc III
+    public int VertexColoring(Graph graph, out int[] colors)
+        {
+        colors = null;
+        return -1;
+        }
+
+    // czesc IV
+    public int StrongEdgeColoring(Graph graph, out Graph coloredGraph)
+        {
+        coloredGraph = null;
+        return -1;
+        }
+
+    }
+
+}
