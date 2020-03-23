2093121b079eeddfaf423a4336d13fd97d8ea5d6
Palina Hrynko <01133991@pw.edu.pl>
2020-03-24 00:16:08 +0100

    Line graph implemented.

--- lab3/Lab03.cs
+++ lab3/Lab03.cs
@@ -36,16 +36,70 @@ using ASD.Graphs;
             return output_graph;
         }
 
-    // czesc II
-    public Graph LineGraph(Graph graph, out (int x, int y)[] names)
+        // czesc II
+        public Graph LineGraph(Graph graph, out (int x, int y)[] names)
         {
-        names = null;
-        return null;
+                Dictionary<(int, int), int> mapping = new Dictionary<(int, int), int>();
+                names = new (int, int)[graph.EdgesCount];
+                for (int v = 0, i = 0; v < graph.VerticesCount; ++v)
+                {
+                    foreach (Edge e in graph.OutEdges(v))
+                    {
+                        if (e.To > e.From)
+                        {
+                            mapping[(e.From, e.To)] = i;
+                            names[i] = (e.From, e.To);
+                            i++;
+                        }
+                        else if (graph.Directed)
+                        {
+                            mapping[(e.To, e.From)] = i;
+                            names[i] = (e.From, e.To);
+                            i++;
+                        }
+                    }
+                }
+                Graph output_graph = graph.IsolatedVerticesGraph(false, graph.EdgesCount);
+                for (int v = 0; v < graph.VerticesCount; ++v)
+                {
+                if (graph.Directed)
+                {
+                    foreach (Edge f in graph.OutEdges(v))
+                    {
+                        foreach (Edge s in graph.OutEdges(f.To))
+                        {
+                            int from = f.To > f.From ? mapping[(f.From, f.To)] : mapping[(f.To, f.From)];
+                            int to = s.To > s.From ? mapping[(s.From, s.To)] : mapping[(s.To, s.From)];
+
+                            output_graph.AddEdge(from, to);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (Edge f in graph.OutEdges(v))
+                    {
+                        foreach (Edge s in graph.OutEdges(v).Where(e => !e.Equals(f)))
+                        {
+                            int from = f.To > f.From ? mapping[(f.From, f.To)] : mapping[(f.To, f.From)];
+                            int to = s.To > s.From ? mapping[(s.From, s.To)] : mapping[(s.To, s.From)];
+
+                            output_graph.AddEdge(from, to);
+                        }
+                    }
+                }
+            }
+            return output_graph;
         }
 
     // czesc III
     public int VertexColoring(Graph graph, out int[] colors)
         {
+            if (graph.VerticesCount == 0)
+            {
+                colors = new int[0];
+                return 0;
+            }
             if (graph.Directed)
                 throw new ArgumentException();
 
@@ -72,7 +126,7 @@ using ASD.Graphs;
     // czesc IV
     public int StrongEdgeColoring(Graph graph, out Graph coloredGraph)
         {
-        coloredGraph = null;
+            coloredGraph = Square(graph);
         return -1;
         }
 
