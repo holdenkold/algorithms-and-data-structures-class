49681a536faedb25755b9088fe06073be053bcca
Polina Grinko <grinkopolina@gmail.com>
2020-03-31 01:18:05 +0200

    implemented 1 part with linear space complexity

--- lab4/VelocityMeasurements.cs
+++ lab4/VelocityMeasurements.cs
@@ -6,6 +6,19 @@ namespace ASD
 
     public class VelocityMeasurements : System.MarshalByRefObject
     {
+        struct node
+        {
+            public node(int val, int refer, int row)
+            {
+                this.val = val;
+                this.refer = refer;
+                this.row = row;
+            }
+            public int val;
+            public int refer;
+            public int row;
+        };
+
 
         /// <summary>
         /// Metoda zwraca możliwą minimalną i maksymalną wartość prędkości samochodu w momencie wypadku.
@@ -17,56 +30,72 @@ namespace ASD
         public (int minVelocity, int maxVelocity, int nr) FinalVelocities(int[] measurements, out bool[] isBraking)
         {
             isBraking = new bool[measurements.Length];
-
             int maxVelocity = measurements.Sum();
             int target = (int)maxVelocity / 2;
-            int[,] dp = new int[measurements.Length + 1, target + 1];
-            int minVelocity = 0;
+            node[] dp = new node[target + 1];
 
-            for (int i = 1; i < dp.GetLength(0); i++)
+
+            for (int i = 0; i < measurements.Length; i++)
             {
-                var value = measurements[i - 1];
-                for (int j = 1; j < dp.GetLength(1); j++)
+                for (int j = target; j >= measurements[i]; j--)
                 {
-                    if (j < value)
-                        dp[i, j] = dp[i - 1, j];
-                    else
+                    var value = measurements[i];
+                    var prev = dp[j - value].val;
+                    if (prev + value > dp[j].val)
                     {
-                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i - 1, j - value] + value);
+                        dp[j] = new node(prev + value, j - value, i);
                     }
                 }
             }
 
-            int first_subset_sum = dp[dp.GetLength(0) - 1, dp.GetLength(1) - 1];
-            int second_subset_sum = maxVelocity - first_subset_sum;
-            minVelocity = Math.Abs(first_subset_sum - second_subset_sum);
-
-            int last = target;
-            for (int i = measurements.Length; i > 0; i--)
+            int idx = dp.Length - 1;
+            while (dp[idx].refer != idx)
             {
-                var cur = dp[i, last];
-                var prev = dp[i - 1, last];
-                if (dp[i, last] == dp[i - 1, last])
-                    continue;
-                else
-                {
-                    var value = measurements[i - 1];
-                    isBraking[i - 1] = true;
-                    last -= measurements[i - 1];
-                }
+                isBraking[dp[idx].row] = true;
+                idx = dp[idx].refer;
             }
 
+            int minVelocity = Math.Abs(maxVelocity - 2 * dp[target].val);
             return (minVelocity, maxVelocity, measurements.Length);
         }
 
-            /// <summary>
-            /// Metoda zwraca możliwą minimalną i maksymalną wartość prędkości samochodu w trakcie całego okresu trwania podróży.
-            /// </summary>
-            /// <param name="measurements">Tablica zawierające wartości pomiarów urządzenia zainstalowanego w aucie Mateusza</param>
-            /// <returns>Krotka z informacjami o najniższej i najwyższej możliwej prędkości na trasie oraz informacją o numerze pomiaru dla którego najniższe prędkość może być osięgnięta</returns>
-            public (int minVelocity, int maxVelocity, int nr) JourneyVelocities(int[] measurements)
+        /// <summary>
+        /// Metoda zwraca możliwą minimalną i maksymalną wartość prędkości samochodu w trakcie całego okresu trwania podróży.
+        /// </summary>
+        /// <param name="measurements">Tablica zawierające wartości pomiarów urządzenia zainstalowanego w aucie Mateusza</param>
+        /// <returns>Krotka z informacjami o najniższej i najwyższej możliwej prędkości na trasie oraz informacją o numerze pomiaru dla którego najniższe prędkość może być osięgnięta</returns>
+        public (int minVelocity, int maxVelocity, int nr) JourneyVelocities(int[] measurements)
         {
-            return (-1, -1, -1);
+            int maxVelocity = measurements.Sum();
+            int target = (int)maxVelocity / 2;
+            int[] dp = new int[target + 1];
+            int idx = 0;
+            int value;
+            int curMaxVelocity = measurements[0];
+            for (int i = 0; i < measurements.Length; i++)
+            {
+                for (int j = target; j >= measurements[i]; j--)
+                {
+                    value = measurements[i];
+                    var prev = dp[j - value];
+                    if (prev + value > dp[j])
+                        dp[j] = prev + value;
+                }
+            }
+
+            target = (int)(curMaxVelocity / 2);
+            int minVelocity = Math.Abs(2 * dp[target] - curMaxVelocity);
+            for (int i = 0; i < measurements.Length; i++)
+            {
+                curMaxVelocity += measurements[i];
+                target = (int)(curMaxVelocity / 2);
+                if (dp[i] < minVelocity)
+                {
+                    idx = i;
+                    minVelocity = Math.Min(minVelocity, Math.Abs(2 * dp[i] - curMaxVelocity));
+                }
+            }
+            return (minVelocity, measurements.Sum(), idx);
         }
 
     }
