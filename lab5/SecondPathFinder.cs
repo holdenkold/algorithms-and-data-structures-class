4cc8c74b1bdffbd2c634b45dda6257450851c607
Palina Hrynko <01133991@pw.edu.pl>
2020-04-08 01:44:32 +0200

    solution for 1 part (undirected graph)

--- lab5/SecondPathFinder.cs
+++ lab5/SecondPathFinder.cs
@@ -8,75 +8,20 @@ namespace Lab05
 
 public class PathFinder : System.MarshalByRefObject
     {
-        public List<int> CalculatePath(int start, int end, PathsInfo[] paths)
+        public List<Edge> CalculateMinPath(int start, int end, PathsInfo[] paths)
         {
-            List<int> path = new List<int> { end };
-            int p = end;
+            List<Edge> path = new List<Edge>();
+            int p = 0;
             while (paths[p].Last.HasValue)
             {
+                Edge reversed = paths[p].Last.Value;
+                path.Add(new Edge(reversed.To, reversed.From, reversed.Weight));
                 p = paths[p].Last.Value.From;
-                path.Insert(0, p);
-                if (p == start)
+                if (p == end)
                     break;
             }
             return path;
         }
-
-        public List<int> CalculateFullPath(int a, Edge medium, int b, PathsInfo[] paths1, PathsInfo[] paths2)
-        {
-            List<int> path = CalculatePath(a, medium.From, paths1);
-            path.Add(medium.To);
-            var end = CalculatePath(medium.To, b, paths2);
-            path.AddRange(end);
-            return path;
-        }
-
-        public Edge[] GenerateEdgePath(Graph g, List<int> int_path)
-        {
-            Edge[] path = new Edge[int_path.Count - 1];
-            for (int i = 1; i < int_path.Count; i++)
-            {
-                var from = int_path[i - 1];
-                var to = int_path[i];
-                path[i - 1] = new Edge(from, to, g.GetEdgeWeight(from, to));
-            }
-            return path;
-        }
-
-        //public List<int> CalculatePath(int start, int end, PathsInfo[] paths)
-        //{
-        //    List<int> path = new List<int> { end };
-        //    int p = end;
-        //    while (paths[p].Last.HasValue)
-        //    {
-        //        p = paths[p].Last.Value.From;
-        //        path.Insert(0, p);
-        //        if (p == start)
-        //            break;
-        //    }
-        //    return path;
-        //}
-
-        //public List<int> CalculateFullPath(int a, Edge medium, int b, PathsInfo[] paths1, PathsInfo[] paths2)
-        //{
-        //    List<int> path = CalculatePath(a, medium.From, paths1);
-        //    path.Add(medium.To);
-        //    var end = CalculatePath(medium.To, b, paths2);
-        //    path.AddRange(end);
-        //    return path;
-        //}
-
-        //public Edge[] GenerateEdgePath(Graph g, List<int> int_path)
-        //{
-        //    Edge[] path = new Edge[int_path.Count -1];
-        //    for (int i = 1; i < int_path.Count; i++)
-        //    {
-        //        var from = int_path[i - 1];
-        //        var to = int_path[i];
-        //        path[i - 1] = new Edge(from, to, g.GetEdgeWeight(from, to));
-        //    }
-        //    return path;
-        //}
 
         /// <summary>
         /// Algorytm znajdujący drugą pod względem długości najkrótszą ścieżkę między a i b.
@@ -93,101 +38,54 @@ public class PathFinder : System.MarshalByRefObject
         public double? FindSecondShortestPath(Graph g, int a, int b, out Edge[] path)
         {
             path = null;
-            g.DijkstraShortestPaths(a, out PathsInfo[] paths);
+            g.DijkstraShortestPaths(b, out PathsInfo[] paths);
 
-            if (double.IsNaN(paths[b].Dist))
+            if (double.IsNaN(paths[a].Dist))
                 return null;
 
-            List<int> min_path = CalculatePath(a, b, paths);
+            List<Edge> min_path = CalculateMinPath(a, b, paths);
 
-            double min_path_len = paths1[b].Dist;
+            double min_path_len = paths[b].Dist;
             double second_min_path_len = double.MaxValue;
-            List<int> second_min_path = new List<int>();
-            for (int v = 0; v < g.VerticesCount; v++)
-            {
-                if (v == b)
-                    continue;
+            List<Edge> second_min_path = new List<Edge>();
+            List<Edge> second = new List<Edge>();
 
-                foreach (Edge e in g.OutEdges(v))
+            double curr_len = 0;
+
+            foreach (var edge in min_path)
+            {
+                foreach (var e in g.OutEdges(edge.From).Where(x => x != edge))
                 {
-                    var dist = paths1[v].Dist + e.Weight + paths2[e.To].Dist;
-                    if (dist >= min_path_len && dist < second_min_path_len)
+                    if (curr_len + e.Weight + paths[e.To].Dist < second_min_path_len)
                     {
-                        var candidate = CalculateFullPath(a, e, b, paths1, paths2);
-                        if (dist == min_path_len)
+                        second_min_path_len = curr_len + e.Weight + paths[e.To].Dist;
+                        second = new List<Edge>();
+                        foreach (var el in second_min_path)
+                            second.Add(el);
+                        second.Add(e);
+                        int v = e.To;
+                        while (paths[v].Last.HasValue)
                         {
-                            if (!Enumerable.SequenceEqual(second_min_path, min_path))
-                            {
-                                path = GenerateEdgePath(g, candidate);
-                                return min_path_len;
-                            }
+                            Edge reversed = paths[v].Last.Value;
+                            second.Add(new Edge(reversed.To, reversed.From, reversed.Weight));
+                            v = paths[v].Last.Value.From;
                         }
-                        else
+                        if (second_min_path_len == min_path_len)
                         {
-                            second_min_path_len = dist;
-                            second_min_path = candidate;
+                            path = second.ToArray();
+                            return second_min_path_len;
                         }
+
                     }
                 }
-
+                curr_len += edge.Weight;
+                second_min_path.Add(edge);
             }
-
+         
             System.Console.WriteLine("Path len is:" + second_min_path_len.ToString());
             if (second_min_path.Count != 0)
             {
-                path = GenerateEdgePath(g, second_min_path);
-                return second_min_path_len;
-            }
-            else
-                return null;
-        }
-        public double? FindSecondShortestPath1(Graph g, int a, int b, out Edge[] path)
-        {
-            path = null;
-            g.DijkstraShortestPaths(a, out PathsInfo[] paths1);
-            g.DijkstraShortestPaths(b, out PathsInfo[] paths2);
-
-            if (double.IsNaN(paths1[b].Dist))
-                return null;
-
-            List<int> min_path = CalculatePath(a, b, paths1);
-
-            double min_path_len = paths1[b].Dist;
-            double second_min_path_len = double.MaxValue;
-            List<int> second_min_path = new List<int>();
-            for(int v = 0; v < g.VerticesCount; v++)
-            {
-                if (v == b)
-                    continue;
-
-                foreach (Edge e in g.OutEdges(v))
-                {
-                    var dist = paths1[v].Dist + e.Weight + paths2[e.To].Dist;
-                    if (dist >= min_path_len && dist < second_min_path_len)
-                    {
-                        var candidate = CalculateFullPath(a, e, b, paths1, paths2);
-                        if (dist == min_path_len)
-                        {
-                            if (!Enumerable.SequenceEqual(second_min_path, min_path))
-                            {
-                                path = GenerateEdgePath(g, candidate);
-                                return min_path_len;
-                            }
-                        }
-                        else
-                        {
-                            second_min_path_len = dist;
-                            second_min_path = candidate;
-                        }
-                    }
-                }
-                
-            }
-
-            System.Console.WriteLine("Path len is:" + second_min_path_len.ToString());
-            if (second_min_path.Count != 0)
-            {
-                path = GenerateEdgePath(g, second_min_path);
+                path = second.ToArray();
                 return second_min_path_len;
             }
             else
@@ -213,5 +111,4 @@ public class PathFinder : System.MarshalByRefObject
         }
 
     }
-
 }
