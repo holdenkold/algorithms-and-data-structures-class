ae390bd629314267383cb233624527148e84aa35
Palina Hrynko <01133991@pw.edu.pl>
2020-05-11 21:14:37 +0200

    first part implemented

--- lab10/ProductionPlanner.cs
+++ lab10/ProductionPlanner.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http.Headers;
 using ASD.Graphs;
 
 namespace ASD
@@ -45,9 +48,62 @@ namespace ASD
         public (int Quantity, double Value) CreateSimplePlan((int Quantity, double Value)[] production, (int Quantity, double Value)[] sales,
             (int Quantity, double Value) storageInfo, out (int UnitsProduced, int UnitsSold, int UnitsStored)[] weeklyPlan)
         {
-            weeklyPlan = null;
-            return (-1, -1.0);
+            int weeks = production.Length;
+            int source, target;
+
+            (Graph fabric_production, Graph fabric_profit) = CreateFlowGraphs(weeks, production, sales, storageInfo, out source, out target);
+            (double value, double min_cost, Graph flow) = fabric_production.MinCostFlow(fabric_profit, source, target, true, MaxFlowGraphExtender.PushRelabelMaxFlow); //OriginalDinicBlockingFlow
+
+            weeklyPlan = CreateWeeklyPlan(weeks, flow, source, target);
+            return ((int)value, -min_cost);
         }
+
+        public (Graph, Graph) CreateFlowGraphs(int weeks, (int Quantity, double Value)[] production, (int Quantity, double Value)[] sales, (int Quantity, double Value) storageInfo, out int source, out int target)
+        {
+            Graph fabric_production = new AdjacencyListsGraph<HashTableAdjacencyList>(true, weeks + 2);
+            Graph fabric_profit = new AdjacencyListsGraph<HashTableAdjacencyList>(true, weeks + 2);
+
+            source = 0;
+            target = weeks + 1;
+
+            for (int i = source + 1; i < weeks; i++)
+            {
+                fabric_production.AddEdge(0, i, production[i - 1].Quantity);
+                fabric_production.AddEdge(i, target, sales[i - 1].Quantity);
+
+                fabric_profit.AddEdge(0, i, production[i - 1].Value);
+                fabric_profit.AddEdge(i, target, -sales[i - 1].Value);
+
+                fabric_production.AddEdge(i, i + 1, storageInfo.Quantity);
+                fabric_profit.AddEdge(i, i + 1, storageInfo.Value);
+            }
+
+            // Adding edge cases
+            fabric_production.AddEdge(source, weeks, production[weeks - 1].Quantity);
+            fabric_production.AddEdge(weeks, target, sales[weeks - 1].Quantity);
+
+            fabric_profit.AddEdge(source, weeks, production[weeks - 1].Value);
+            fabric_profit.AddEdge(weeks, target, -sales[weeks - 1].Value);
+
+            return (fabric_production, fabric_profit);
+        }
+
+        public (int UnitsProduced, int UnitsSold, int UnitsStored)[] CreateWeeklyPlan(int weeks, Graph flow, int source, int target)
+        {
+            var weeklyPlan = new (int UnitsProduced, int UnitsSold, int UnitsStored)[weeks];
+
+            for (int i = source + 1; i < weeks; i++)
+            {
+                weeklyPlan[i - 1].UnitsProduced = (int)flow.GetEdgeWeight(source, i);
+                weeklyPlan[i - 1].UnitsSold = (int)flow.GetEdgeWeight(i, target);
+                weeklyPlan[i - 1].UnitsStored = (int)flow.GetEdgeWeight(i, i + 1);
+            }
+
+            weeklyPlan[weeks - 1].UnitsProduced = (int)flow.GetEdgeWeight(source, weeks);
+            weeklyPlan[weeks - 1].UnitsSold = (int)flow.GetEdgeWeight(weeks, target);
+            return weeklyPlan;
+        }
+
 
         /// <summary>
         /// Część 2. zadania - zaplanowanie produkcji telewizorów dla wielu kontrahentów.
@@ -88,6 +144,7 @@ namespace ASD
             return (-1, -1.0);
         }
 
-    }
 
+
+    }
 }
