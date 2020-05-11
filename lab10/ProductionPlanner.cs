9872eda78f610ebd68becab36c84268955d5fa40
Palina Hrynko <01133991@pw.edu.pl>
2020-05-12 00:00:46 +0200

    second part implemented

--- lab10/ProductionPlanner.cs
+++ lab10/ProductionPlanner.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Net.Http.Headers;
 using ASD.Graphs;
 
 namespace ASD
@@ -48,6 +47,7 @@ namespace ASD
         public (int Quantity, double Value) CreateSimplePlan((int Quantity, double Value)[] production, (int Quantity, double Value)[] sales,
             (int Quantity, double Value) storageInfo, out (int UnitsProduced, int UnitsSold, int UnitsStored)[] weeklyPlan)
         {
+            CheckArguments(production, sales, storageInfo);
             int weeks = production.Length;
             int source, target;
 
@@ -56,6 +56,24 @@ namespace ASD
 
             weeklyPlan = CreateWeeklyPlan(weeks, flow, source, target);
             return ((int)value, -min_cost);
+        }
+        public void CheckArguments((int Quantity, double Value)[] production, (int Quantity, double Value)[] sales, (int Quantity, double Value) storageInfo)
+        {
+            if(production.Length == 0)
+                throw new ArgumentException("0 weeks");
+
+            if (production.Length != sales.Length)
+                throw new ArgumentException("input array have different length");
+
+            if (production.Any(p => p.Value < 0))
+                throw new ArgumentException("fabric has negative value");
+
+            if (storageInfo.Value < 0)
+                throw new ArgumentException("storage has negative cost");
+
+            if (sales.Any(s => s.Value < 0))
+                throw new ArgumentException("sale has negative cost");
+
         }
 
         public (Graph, Graph) CreateFlowGraphs(int weeks, (int Quantity, double Value)[] production, (int Quantity, double Value)[] sales, (int Quantity, double Value) storageInfo, out int source, out int target)
@@ -68,10 +86,10 @@ namespace ASD
 
             for (int i = source + 1; i < weeks; i++)
             {
-                fabric_production.AddEdge(0, i, production[i - 1].Quantity);
+                fabric_production.AddEdge(source, i, production[i - 1].Quantity);
                 fabric_production.AddEdge(i, target, sales[i - 1].Quantity);
 
-                fabric_profit.AddEdge(0, i, production[i - 1].Value);
+                fabric_profit.AddEdge(source, i, production[i - 1].Value);
                 fabric_profit.AddEdge(i, target, -sales[i - 1].Value);
 
                 fabric_production.AddEdge(i, i + 1, storageInfo.Quantity);
@@ -140,11 +158,105 @@ namespace ASD
         public (int Quantity, double Value) CreateComplexPlan((int Quantity, double Value)[] production, (int Quantity, double Value)[,] sales, (int Quantity, double Value) storageInfo,
             out (int UnitsProduced, int[] UnitsSold, int UnitsStored)[] weeklyPlan)
         {
-            weeklyPlan = null;
-            return (-1, -1.0);
+            CheckArguments(production, sales, storageInfo);
+            int weeks = production.Length;
+            int contractors = sales.GetLength(0);
+
+            int source, target;
+            (Graph fabric_production, Graph fabric_profit) = CreateFlowGraphs(weeks, production, sales, storageInfo, out source, out target);
+            (double value, double min_cost, Graph flow) = fabric_production.MinCostFlow(fabric_profit, source, target, true, MaxFlowGraphExtender.PushRelabelMaxFlow); //OriginalDinicBlockingFlow
+
+            weeklyPlan = CreateWeeklyPlan(weeks, contractors, flow, source, target);
+            int quantity = Enumerable.Range(0, weeks).Sum(i => (int)flow.GetEdgeWeight(i + 1, weeks + i + 1));            
+            return (quantity, -min_cost);
         }
 
+        public void CheckArguments((int Quantity, double Value)[] production, (int Quantity, double Value)[,] sales, (int Quantity, double Value) storageInfo)
+        {
+            if (production.Length == 0)
+                throw new ArgumentException("0 weeks");
 
+            if(sales.GetLength(0) == 0)
+                throw new ArgumentException("0 sales");
+
+            if (production.Any(p => p.Value < 0))
+                throw new ArgumentException("fabric has negative value");
+
+            if (storageInfo.Value < 0)
+                throw new ArgumentException("storage has negative cost");
+
+            foreach (var s in sales)
+            {
+                if(s.Value < 0)
+                    throw new ArgumentException("sale has negative cost");
+            }
+        }
+
+        public (Graph, Graph) CreateFlowGraphs(int weeks, (int Quantity, double Value)[] production, (int Quantity, double Value)[,] sales, (int Quantity, double Value) storageInfo, out int source, out int target)
+        {
+            Graph fabric_production = new AdjacencyListsGraph<HashTableAdjacencyList>(true, 2 * weeks + sales.GetLength(0) + 2);
+            Graph fabric_profit = new AdjacencyListsGraph<HashTableAdjacencyList>(true, 2 * weeks + sales.GetLength(0) + 2);
+
+            source = 0;
+            target = sales.GetLength(0) + 2 * weeks + 1;
+
+            for (int i = source + 1; i < weeks; i++)
+            {
+                fabric_production.AddEdge(source, i, production[i - 1].Quantity);
+                fabric_production.AddEdge(i, target, production[i - 1].Quantity);
+
+                fabric_profit.AddEdge(source, i, 0);
+                fabric_profit.AddEdge(i, target, 0);
+
+                fabric_production.AddEdge(weeks + i, weeks + i + 1, storageInfo.Quantity);
+                fabric_profit.AddEdge(weeks + i, weeks + i + 1, storageInfo.Value);                
+            }
+
+            fabric_production.AddEdge(source, weeks, production[weeks - 1].Quantity);
+            fabric_production.AddEdge(weeks, target, production[weeks - 1].Quantity);
+
+            fabric_profit.AddEdge(source, weeks, 0);
+            fabric_profit.AddEdge(weeks, target, 0);
+
+            for (int i = source + 1; i < weeks + 1; i++)
+            {
+                fabric_production.AddEdge(i, i + weeks, production[i - 1].Quantity);
+                fabric_profit.AddEdge(i, i + weeks, production[i - 1].Value);
+                for (int j = 0; j < sales.GetLength(0); j++)
+                {
+                    fabric_production.AddEdge(weeks + i, 2 * weeks + 1 + j, sales[j, i - 1].Quantity);
+                    fabric_production.AddEdge(2 * weeks + 1 + j, target, double.MaxValue);
+
+                    fabric_profit.AddEdge(weeks + i, 2 * weeks + 1 + j, -sales[j, i - 1].Value);
+                    fabric_profit.AddEdge(2 * weeks + 1 + j, target, 0);
+                }
+            }
+
+            return (fabric_production, fabric_profit);
+        }
+
+        public (int UnitsProduced, int[] UnitsSold, int UnitsStored)[] CreateWeeklyPlan(int weeks,int contractors, Graph flow, int source, int target)
+        {
+            var weeklyPlan = new (int UnitsProduced, int[] UnitsSold, int UnitsStored)[weeks];
+
+            for (int i = 0; i < weeks; i++)
+                weeklyPlan[i].UnitsSold = new int[contractors];
+
+            for (int i = source + 1; i < weeks; i++)
+            {
+                weeklyPlan[i - 1].UnitsProduced = (int)flow.GetEdgeWeight(i, weeks + i);
+                weeklyPlan[i - 1].UnitsStored = (int)flow.GetEdgeWeight(weeks + i, weeks + i + 1);
+                for (int j = 0; j < contractors; j++)
+                    weeklyPlan[i-1].UnitsSold[j] = (int)flow.GetEdgeWeight(weeks + i, 2 * weeks + j + 1);
+            }
+
+            // Egde cases
+            weeklyPlan[weeks - 1].UnitsProduced = (int)flow.GetEdgeWeight(weeks, 2*weeks);
+            for (int j = 0; j < contractors; j++)
+                weeklyPlan[weeks - 1].UnitsSold[j] = (int)flow.GetEdgeWeight(2*weeks, 2*weeks + j + 1);
+
+            return weeklyPlan;
+        }
 
     }
 }
